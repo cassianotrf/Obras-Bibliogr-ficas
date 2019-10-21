0dd752f15ad82f6f48570c7f5baadfab28ac6f85
Cassiano Mangini <cassiano.mangini@gmail.com>
2019-10-21 15:54:07 -0300

    Ajuste final

--- ruby_puro/test.rb
+++ ruby_puro/test.rb
@@ -4,24 +4,22 @@ authors = Author.new
 
 x = 1
 nomes = []
-qtdade_nomes = 0
+qtdade_nomes = ''
 
-until qtdade_nomes == 'sair'
-  puts 'Digite a quantidade de nomes que serão registrados (Digite sair para finalizar a sessão):'
-  qtdade_nomes = gets.chomp
-  qtdade_nomes.to_i.times do
-    puts "Digite o nome da pessoa #{x}:"
-    nomes.push(gets.chomp)
-    x += 1
-  end
-  x = 1
-  nomes_formatados = authors.format_names(qtdade_nomes.to_i,nomes)
-  puts
-  puts 'Lista de Pessoas'
-  puts
-  nomes_formatados.each do |nome|
-    puts "Pessoa #{x}: #{nome}"
-    x += 1
-  end
-  puts
+puts 'Digite a quantidade de nomes que serão registrados:'
+qtdade_nomes = gets.chomp
+qtdade_nomes.to_i.times do
+  puts "Digite o nome da pessoa #{x}:"
+  nomes.push(gets.chomp)
+  x += 1
 end
+x = 1
+nomes_formatados = authors.format_names(qtdade_nomes.to_i,nomes)
+puts
+puts 'Lista de Pessoas'
+puts
+nomes_formatados.each do |nome|
+  puts "Pessoa #{x}: #{nome}"
+  x += 1
+end
+puts
